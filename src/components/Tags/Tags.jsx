8b9faf63d390b4433d19d85c1a9bb8924103e56a
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-10-22 22:58:19 +0200

    Removed defaultValue on Tags when multiple selection

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -34,13 +34,18 @@ export default class Tags extends React.Component {
     render(){
         const { tags } = this.state;
         const multipleSelect = this.props.multiple ? 'multiple' : '';
+        const defaultValue = this.props.multiple ? [] : 'DEFAULT';
         return (
             <div>
                 {
                 tags
                 &&
-                <select defaultValue="DEFAULT" multiple={multipleSelect} onChange={this.onChange}>
-                    <option value="DEFAULT" disabled>Select a tag</option>
+                <select defaultValue={defaultValue} multiple={multipleSelect} onChange={this.onChange}>
+                    {
+                        !multipleSelect
+                        &&
+                        <option value="DEFAULT" disabled>Select a tag</option>
+                    }
                     { this.renderTags(tags) }
                 </select>
                 }
