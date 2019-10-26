1af46cac3f15f491b9a09afde228d235477aa5e4
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-10-26 14:42:55 +0200

    AdvertList removed dummy zero result

--- src/components/AdvertList/AdvertList.jsx
+++ src/components/AdvertList/AdvertList.jsx
@@ -6,11 +6,7 @@ import Pagination from "../Pagination/Pagination";
 import './AdvertList.css';
 
 export default class AdvertList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
+  
   buildAdvertList = adverts => {
     return (
       <div className="adverts-container">
@@ -23,6 +19,7 @@ export default class AdvertList extends React.Component {
 
   render() {
     const adverts = this.props.adverts.results;
+
     return (
       <div>
         {
@@ -30,7 +27,10 @@ export default class AdvertList extends React.Component {
           &&
           adverts.length
           && 
-          this.buildAdvertList(adverts)
+          adverts.count !== 0 ?
+            this.buildAdvertList(adverts)
+          :
+            ''
         }
 
         <Pagination />
