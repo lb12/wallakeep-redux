2c2663096e2d81e00ca58e5e4fa5b74160affc44
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-10-26 14:08:27 +0200

    Not found page component styled and RWD

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,16 +1,16 @@
 import React from "react";
+import './NotFoundPage.css';
+
+import {Link} from 'react-router-dom';
 
 export default class NotFoundPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
-      <div>
-        <h1>404</h1> <h2>Oops, something went wrong.</h2>
-        <p>This page does not exist.</p>
+      <div className="not-found-page">
+        <h1>404</h1> 
+        <h2 className="text-center">Oops, something went wrong.</h2>
+        <p className="text-center">This advert page does not exist.</p>
+        <Link to="/">Go home</Link>
       </div>
     );
   }
