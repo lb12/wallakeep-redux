4c000a4db3e92783080c4be3aaee4074d048c8b5
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-10-27 22:38:59 +0100

    Navbar width fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -59,7 +59,7 @@ export default class App extends React.Component {
         <ErrorBoundary>
           <UserContext.Provider value={value}>        
             <Router>
-              <div id="main">
+              
                 {
                   !this.isUserLogged()
                   &&
@@ -72,7 +72,7 @@ export default class App extends React.Component {
                   &&
                   <React.Fragment>              
                     <Navbar/>
-
+                    <div id="main">
                       <Switch>
                         <Route path="/login" component={Login} />
                         <Route path="/profile" component={Profile} />
@@ -82,9 +82,9 @@ export default class App extends React.Component {
                         <Route exact path="/" component={Home} />
                         <Route component={NotFoundPage} />
                       </Switch>
+                    </div>
                 </React.Fragment>
                 }
-              </div>
             </Router>
           </UserContext.Provider>
         </ErrorBoundary>
