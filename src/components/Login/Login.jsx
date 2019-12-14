b7eba8383ee406358abf48719fbde27a2d5778ce
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-12-14 01:44:08 +0100

    Form and Input component created using HOC and context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,74 +2,52 @@ import React from "react";
 
 import Tags from "../Tags";
 import './Login.css';
+import Form from "../Form";
+import Input from "../Input";
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      user: { 
-        firstname: '', 
-        surname: '', 
-        tag: '' 
-      }
+      tag: ''
     };
   }
   
-  onSubmit = evt => {
-    evt && evt.preventDefault();
-    this.props.setUser(this.state.user); // Save user into redux-store
+  onSubmit = inputs => {
+    const { firstname, surname } = inputs;
+    const user = { firstname, surname, tag: this.state.tag };
+    
+    this.props.setUser(user); // Save user into redux-store
     this.props.history.push('/'); //  Redirect user to home page always
   };
 
-  onInputChange = evt => {
-    const { name, value } = evt.target;
 
-    this.setState(({ user }) => ({
-      user: {
-        ...user,
-        [name]: value
-      }
-    }));
-  };
-
-  onTagSelected = optionSelected => {
-    let user = this.state.user;
-    user.tag = optionSelected;
-
-    this.setState({ user });
-  };
+  onTagSelected = optionSelected => ( this.setState({ tag: optionSelected }) );
 
   render() {
-    const { firstname, surname } = this.state.user;
     return (
       <div className="container login">
         <h1>Login</h1>
-        <form className="mt-4" onSubmit={this.onSubmit}>
+        <Form className="mt-4" onSubmit={this.onSubmit}>
           <div className="form-group">
             <label htmlFor="firstname">Firstname</label>
-            <input
+            <Input
               type="text"
               required
               name="firstname"
-              value={firstname}
               className="form-control"
               placeholder="Enter firstname"
-              onChange={this.onInputChange}
-              id=""
             />
           </div>
           <div className="form-group">
             <label htmlFor="surname">Surname</label>
-            <input
+            <Input 
               type="text"
-              required
+              required="required"
               name="surname"
-              value={surname}
               className="form-control"
               placeholder="Enter surname"
-              onChange={this.onInputChange}
-              id=""
             />
           </div>
           <div className="form-group">
@@ -77,7 +55,7 @@ export default class Login extends React.Component {
             <Tags required={true} onTagSelected={this.onTagSelected} />
           </div>
           <button type="submit" className="btn btn-primary mt-1">Login</button>
-        </form>
+        </Form>
       </div>
     );
   }
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 
 import { setUser } from '../../store/actions';
 import Login from './Login';
+import withForm from '../Form/withForm';
 
 const mapDispatchToProps = dispatch => ({
     setUser: user => dispatch( setUser(user) )
 });
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(null, mapDispatchToProps)(withForm(Login));
