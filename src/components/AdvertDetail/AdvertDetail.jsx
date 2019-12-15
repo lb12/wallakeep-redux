27aeebf11364a6e319416737991abccf6a54bcf1
David Escribano Rodriguez <david.escribanor@gmail.com>
2019-12-15 13:17:55 +0100

    AdvertDetail component refactored using hooks

--- src/components/AdvertDetail/AdvertDetail.jsx
+++ src/components/AdvertDetail/AdvertDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import * as API from '../../services/APIService';
@@ -6,6 +6,62 @@ import Advert from "../Advert";
 
 import './AdvertDetail.css';
 import NotFoundPage from "../NotFoundPage";
+
+function AdvertDetail(props) {
+  const [advert, setAdvert] = useState(null);
+  const [advertError, setAdvertError] = useState(false);  
+  const source = axios.CancelToken.source();
+  const advertId = props.match.params.id;
+
+  useEffect(() => {
+    const getAdvert = async () => {
+      if (advert) return;
+  
+      let _advert = await API.getAdvertById(advertId, source);
+  
+      if ( !_advert || !_advert.success ) {
+        setAdvertError(true);
+        return;
+      }
+      
+      _advert = _advert.result;
+      setAdvert(_advert);
+    };
+
+    getAdvert();
+
+    return function cleanup() {
+      // Cancela cualquier peticion que no se haya podido completar debido a que el componente se haya desmontado
+      source.cancel('AdvertDetail component');
+    }
+  });
+  
+
+  const editAdvert = () => {
+    props.history.push(`/edit-advert/${advertId}`);
+  };
+
+  return (
+    <React.Fragment>
+      {
+        !advertError && advert ?
+        <div className="detail">
+          <Advert advert={advert} />
+          <button className="btn btn-primary edit-ad-submit-btn" onClick={editAdvert}>Edit advert</button>
+        </div>
+        :
+        <NotFoundPage />
+      }
+    </React.Fragment>
+  );
+}
+
+export default AdvertDetail;
+
+
+/* 
+
+// NOTA: Dejo el componente que habia antes de refactorizarlo con hooks.
 
 export default class AdvertDetail extends React.Component {
   constructor(props) {
@@ -62,3 +118,4 @@ export default class AdvertDetail extends React.Component {
     );
   }
 }
+*/
